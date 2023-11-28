976497f0f57a660c3818d377f0b7413cd1e82ba2
Zion <zionadewalex@gmail.com>
2023-11-28 18:30:01 +0000

    Created generateQuestionString function.

--- tests/purpmaths.test.js
+++ tests/purpmaths.test.js
@@ -1,24 +1,47 @@
-const timeTableNumbers = []
+const timeTableNumbers = [1, 2, 5, 10] //Should be null after we finish testing.
 
-function randomNumberFrom1to12() {
-    let number = Math.floor((Math.random() * 12) + 1)
+function randomNumberFrom1toX(x) {
+    let number = Math.floor((Math.random() * x) + 1)
     return number
 }
 
+function randomNumberFrom0toX(x) {
+    let number = Math.floor(Math.random() * (x + 1))
+    return number
+}
 
-describe("randomNumber1to12()", () => {
+function generateQuestionString() {
+    let timeTableNumber = timeTableNumbers[randomNumberFrom0toX(timeTableNumbers.length)] 
+    question = `${timeTableNumber} x ${randomNumberFrom1toX(12)}`
+    return question
+}
+
+describe("generateQuestionString()", () => {
     it("is truthy?", () => {
-        let num = randomNumberFrom1to12()
+        let question = generateQuestionString()
+        expect(question).toBeTruthy()
+    })
+})
+
+
+describe("randomNumber1toX()", () => {
+    it("is truthy?", () => {
+        let num = randomNumberFrom1toX(12)
         expect(num).toBeTruthy()
     })
 
     it("is not less than 1?", () => {
-        let num = randomNumberFrom1to12()
+        let num = randomNumberFrom1toX(12)
         expect(num).not.toBeLessThan(1)
     })
 
-    it("is not greater than 12?", () => {
-        let num = randomNumberFrom1to12()
+    it("is not greater than x?", () => {
+        let num = randomNumberFrom1toX(12)
         expect(num).not.toBeGreaterThan(12)
+    })
+
+    it("is working for the value of 4", () => {
+        let num = randomNumberFrom1toX(4)
+        expect(num).not.toBeGreaterThan(4)
     })
 })
