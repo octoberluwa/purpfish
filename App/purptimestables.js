d433a857f0e342048ae8ea58673fa31ed6e47b21
Zion <zionadewalex@gmail.com>
2023-12-13 16:25:38 +0000

    Created a function to show the question and answer on screen.

--- App/purptimestables.js
+++ App/purptimestables.js
@@ -1,3 +1,6 @@
+const TIMES_TABLE_QUESTION = document.getElementById("times-table-question")
+const TIMES_TABLE_ANSWER = document.getElementById("times-table-answer")
+
 const timeTableNumbers = [1, 2, 5, 10] //Should be null after we finish testing.
 let randomNumberFrom1to12
 let timeTableNumber
@@ -17,7 +20,7 @@ function generateProductPair() {
     timeTableNumber = randomElementFromArray(timeTableNumbers)
     randomNumberFrom1to12 = randomNumberFrom1toX(12)
 }
-function generateQuestionString() {
+function generateQuestion() {
     let question = `${timeTableNumber} x ${randomNumberFrom1to12}?`
     return question
 }
@@ -27,3 +30,8 @@ function generateAnswer() {
     return answer
 }
 
+function presentQuestion() {
+    generateProductPair()
+    TIMES_TABLE_ANSWER.innerText = generateAnswer()
+    TIMES_TABLE_QUESTION.innerText = generateQuestion()
+}
