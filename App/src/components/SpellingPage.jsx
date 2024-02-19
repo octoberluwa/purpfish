8edf31ee97371635d81383711805420b2719afe8
Zion <zionadewalex@gmail.com>
2024-02-19 14:47:10 +0000

    Added input element for custom words to spell

--- App/src/components/SpellingPage.jsx
+++ App/src/components/SpellingPage.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 const SpellingPage = () => {
+  const [WordList, setWordList] = useState([])
+  const [IsSpellingSettingsSubmitted, setIsSpellingSettingsSubmitted] = useState(false)
+
+  const wordToSubmit = useRef(null)
+
+  function submitWord() {
+    if (wordToSubmit.current.value == "") { return } 
+
+    setWordList([...WordList, wordToSubmit.current.value])
+    wordToSubmit.current.value = ""
+  }
+
+  function submitWordList() {
+    setIsSpellingSettingsSubmitted(true)
+  }
+
+  
   return (
     <>
-        <div>working</div>
+        <main>
+          {!IsSpellingSettingsSubmitted && <div id="spelling-settings">
+            <input type="text" ref={wordToSubmit}></input>
+            <div id="spelling-settings-buttons">
+              <button onClick={submitWord}>Submit</button>
+              {!(WordList == ![]) && <button onClick={submitWordList}>Next</button>}
+            </div>
+            <p>{ WordList }</p>
+            <p>
+              Work in Progress.
+            </p>
+          </div>}
+        </main>
     </>
     )
 }
