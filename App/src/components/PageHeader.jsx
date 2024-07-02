a0ab47a3efb8eb5e56bca23981c38d5c7c451c1c
Zion <zionadewalex@gmail.com>
2024-07-02 10:55:14 +0100

    Updated Node Modules.

--- App/src/components/PageHeader.jsx
+++ App/src/components/PageHeader.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const PageHeader = () => {
   return (
     <header>
+        <img src="../src/images/purpfish_logo.png" id="purpfish-logo"/>
         <h1 id="main-header">purpfish</h1>
     </header>
   )
